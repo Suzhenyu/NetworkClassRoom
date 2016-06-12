076001b0706954a5d93c612b453fccd7b2720539
Suzhenyu <983424749@qq.com>
2016-06-12 17:05:36 +0800

    即时通讯先做到这。下面做教师部分去。

---
+++ NetworkClassRoom/SelectChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectChatViewController.h
+//  NetworkClassRoom
+//
+//  Created by apple on 16/6/12.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectChatViewController : UIViewController
+
+@end
--- NetworkClassRoom/StudentOperationController.m
+++ NetworkClassRoom/StudentOperationController.m
@@ -9,6 +9,7 @@
 #import "StudentOperationController.h"
 #import <RongIMKit/RongIMKit.h>
 #import <CommonCrypto/CommonDigest.h>
+#import "Student.h"
 
 @interface StudentOperationController ()<RCIMUserInfoDataSource>
 
@@ -20,6 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    //获取token
     [self getToken];
     
     //设置需要显示哪些类型的会话
@@ -32,12 +34,23 @@
     //设置需要将哪些类型的会话在会话列表中聚合显示
     [self setCollectionConversationType:@[@(ConversationType_DISCUSSION),
                                           @(ConversationType_GROUP)]];
+    
+//    UIBarButtonItem *rightItem
+//    = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"chat"]
+//                                       style:UIBarButtonItemStylePlain
+//                                      target:self
+//                                      action:@selector(gotoSelectChatAction)];
+//    self.tabBarController.navigationItem.rightBarButtonItem = rightItem;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
     self.tabBarController.title = @"作业区";
+}
+
+- (void)gotoSelectChatAction {
+    
 }
 
 - (void)onSelectedTableRow:(RCConversationModelType)conversationModelType
@@ -50,11 +63,15 @@
     //设置会话的目标会话ID。（单聊、客服、公众服务会话为对方的ID，讨论组、群聊、聊天室为会话的ID）
     chat.targetId = model.targetId;
     //设置聊天会话界面要显示的标题
-    chat.title = @"Test2";
+    chat.title = model.conversationTitle;
+    
     //显示聊天会话界面
     [self.navigationController pushViewController:chat animated:YES];
 }
 
+/**
+ * 获取token并连接服务器
+ */
 - (void)getToken {
     NSURL *url = [NSURL URLWithString:@"https://api.cn.ronghub.com/user/getToken.json"];
     NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:url];
@@ -72,8 +89,11 @@
     [request setValue:nonce forHTTPHeaderField:@"Nonce"];
     [request setValue:timestamp forHTTPHeaderField:@"Timestamp"];
     //设置请求体
+    NSData *studentData = [[NSUserDefaults standardUserDefaults] objectForKey:kUserDefaults_Student];
+    Student *student = [NSKeyedUnarchiver unarchiveObjectWithData:studentData];
+    
     NSString *httpBody
-    = [NSString stringWithFormat:@"userId=%@&name=%@&portraitUri=%@", @"1", @"Suyu", @"http://121.42.162.159/upload/123.png"];
+    = [NSString stringWithFormat:@"userId=%@&name=%@&portraitUri=%@", [NSString stringWithFormat:@"s%i",student.student_id], student.student_name, nil];
     [request setHTTPBody:[httpBody dataUsingEncoding:NSUTF8StringEncoding]];
     
     NSURLSession *session = [NSURLSession sharedSession];
@@ -107,21 +127,36 @@
 /**
  *此方法中要提供给融云用户的信息，建议缓存到本地，然后改方法每次从您的缓存返回
  */
+/**
+ * 1.这个用户信息最好缓存道本地；
+ * 2.为保存同步，最好是在服务端设一个迭代码
+ */
 - (void)getUserInfoWithUserId:(NSString *)userId completion:(void(^)(RCUserInfo* userInfo))completion
 {
-    //此处为了演示写了一个用户信息
-    if ([@"1" isEqual:userId]) {
+    if ([@"t1" isEqual:userId]) {
         RCUserInfo *user = [[RCUserInfo alloc]init];
-        user.userId = @"1";
-        user.name = @"测试1";
+        user.userId = @"t1";
+        user.name = @"孙丽珺";
         user.portraitUri = @"https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=1756054607,4047938258&fm=96&s=94D712D20AA1875519EB37BE0300C008";
         
         return completion(user);
-    }else if([@"2" isEqual:userId]) {
+    }else if([@"t2" isEqual:userId]) {
         RCUserInfo *user = [[RCUserInfo alloc]init];
         user.userId = @"2";
-        user.name = @"测试2";
-        user.portraitUri = @"https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=1756054607,4047938258&fm=96&s=94D712D20AA1875519EB37BE0300C008";
+        user.name = @"陈琦";
+        user.portraitUri = @"http://121.42.162.159/upload/123.png";
+        return completion(user);
+    }else if([@"t3" isEqual:userId]) {
+        RCUserInfo *user = [[RCUserInfo alloc]init];
+        user.userId = @"t3";
+        user.name = @"李勤";
+        user.portraitUri = @"http://121.42.162.159/upload/123.png";
+        return completion(user);
+    }else if([@"s1" isEqual:userId]) {
+        RCUserInfo *user = [[RCUserInfo alloc]init];
+        user.userId = @"s1";
+        user.name = @"宿振宇";
+        user.portraitUri = @"http://121.42.162.159/upload/123.png";
         return completion(user);
     }
 }
